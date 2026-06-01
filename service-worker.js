31a6afc0357d2e077a20997ae1b0de523b7fed86
Robinson Obwocha <robinsonobwocha10@gmail.com>
2026-06-01 23:15:36 +0300

    feat: SEO meta+OG tags, PWA manifest+SW, search filter, skeleton loading, placeholder SVG images

---
+++ service-worker.js
@@ -0,0 +1,55 @@
+const CACHE = 'obwocha-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/shop.html',
+  '/cart.html',
+  '/checkout.html',
+  '/about.html',
+  '/contact.html',
+  '/faq.html',
+  '/blog.html',
+  '/brands.html',
+  '/services.html',
+  '/store-locator.html',
+  '/health-conditions.html',
+  '/prescription.html',
+  '/product.html',
+  '/account.html',
+  '/admin.html',
+  '/css/style.css',
+  '/js/cart.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const fetchPromise = fetch(e.request).then(res => {
+        if (res.ok && res.type === 'basic') {
+          const clone = res.clone();
+          caches.open(CACHE).then(cache => cache.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
